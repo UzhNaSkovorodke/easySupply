051c3115d94824b77242561c6da5e92cf55b517a
ktiukalov <tiukalov@realx.tech>
2024-03-28 17:56:54 +0300

    feat/добавил тестовую обертку

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import styles from './page.module.css'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main}>Main</main>
+    <main className={styles.main}>
+      <div className={styles.root}>
+          <Image src={'https://rostline.com/wp-content/uploads/2019/12/slayder-11-1920x1140.jpg'} alt={'traktor'} width={320} height={320}/>
+      </div>
+    </main>
   )
 }
